52bd512a76190787913f16e7ceec36382eb653bc
terrysky18 <terrysky83@gmail.com>
2017-10-25 08:40:22 -0400

    continue working on unit4 exercise

--- MarksRecording/src/com/EirScientific/Main.java
+++ MarksRecording/src/com/EirScientific/Main.java
@@ -6,7 +6,14 @@ public class Main {
         int[] my_marks = {12, 45, 34, 78, 55, 63};
         MarksArray csm1 = new MarksArray(my_marks);
 
-        System.out.println(my_marks.length);
+        System.out.println(csm1.sizeof());
         System.out.println(csm1);
+
+        System.out.println(csm1.get(0));
+        System.out.println(csm1.get(4));
+        System.out.println(csm1.get(9));
+
+        csm1.append(108);
+        csm1.listMarks();
     }
 }
--- MarksRecording/src/com/EirScientific/MarksArray.java
+++ MarksRecording/src/com/EirScientific/MarksArray.java
@@ -14,10 +14,45 @@ public class MarksArray {
     public int get(int index)
     {
         if (index >= 0 && index < array_length)
+        {
+            return marks[index];
+        }
+        else
+        {
+            return -1;
+        }
     }
-    public int length()
+    public int sizeof()
     {
         return array_length;
+    }
+
+    public void put(int index, int value)
+    {
+        if (index >= 0 && index < array_length)
+        {
+            marks[index] = value;
+        }
+    }
+
+    public void append(int value)
+    {
+        int[] new_array = new int[array_length+1];
+        for (int i=0; i<array_length; i++)
+        {
+            new_array[i] = marks[i];
+        }
+        new_array[array_length] = value;
+        marks = new_array;
+        array_length = marks.length;
+    }
+
+    public void listMarks()
+    {
+        for (int mark : marks)
+        {
+            System.out.println(Integer.toString(mark));
+        }
     }
 
     @Override
