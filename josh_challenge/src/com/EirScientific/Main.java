85a4f69fe7eb8cd053e4a85bf2bf6f99aa9d8ee7
terrysky18 <terrysky83@gmail.com>
2017-11-15 03:56:26 -0500

    completed Josh Bizley challenges

--- josh_challenge/src/com/EirScientific/Main.java
+++ josh_challenge/src/com/EirScientific/Main.java
@@ -6,12 +6,18 @@ public class Main {
         int a = -1;
         int b = 12;
         Challenger my_solution = new Challenger(a, b);
-        my_solution.printNumber();
+        //my_solution.printNumber();
 
         System.out.println();
         a = 3;
         b = 3;
         Challenger my_solution2 = new Challenger(a, b);
-        my_solution2.printNumber();
+        //my_solution2.printNumber();
+
+        FuzzBuzzer big_fuzz = new FuzzBuzzer();
+        for (int i=-10; i<21; i++)
+        {
+            big_fuzz.printNumber(i);
+        }
     }
 }
